8ede09f2a0a3a02326b1e04ea8acb96cbfe7375e
simmimbose <96460197+simmimbose@users.noreply.github.com>
2021-12-21 18:07:45 +0530

    Create loadble.js

---
+++ loadble.js
@@ -0,0 +1,62 @@
+export default {
+    data() {
+        return {
+            isLoading: false,
+            loadingText: '',
+            loadingTask: null,
+            loaders: {}
+        };
+    },
+    methods: {
+        setLoader(loaderName = null, { isLoading, loadingText, loadingTask }) {
+            let loader =
+                loaderName === null
+                    ? this
+                    : this.loaders[loaderName] ||
+                      (this.loaders[loaderName] = {});
+
+            loader.isLoading = isLoading || false;
+            loader.loadingText = loadingText || 'Loading';
+            loader.loadingTask = loadingTask || null;
+            return loader;
+        },
+
+        resetLoader(loaderName = null) {
+            let loader = loaderName === null ? this : this.loaders[loaderName];
+
+            if (!loader) {
+                throw new Error('Loader does not exist!');
+            }
+
+            loader.isLoading = false;
+            loader.loadingText = '';
+            loader.loadingTask = null;
+            return loader;
+        },
+
+        gonnaBe(loadingText = 'Loading') {
+            let loaderName = null;
+
+            let instance = {
+                as: customLoaderName => {
+                    loaderName = customLoaderName;
+                },
+                with: (...tasks) => {
+                    let loadingTask = Promise.all(tasks).finally(() => {
+                        this.resetLoader(loaderName);
+                    });
+
+                    this.setLoader(loaderName, {
+                        isLoading: true,
+                        loadingText,
+                        loadingTask
+                    });
+
+                    return loadingTask;
+                }
+            };
+
+            return instance;
+        }
+    }
+};
